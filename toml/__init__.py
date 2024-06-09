621546d98d5dfc3b9fd3ec40f7a3c8719b98c491
elpekenin <elpekenin@elpekenin.dev>
2024-06-09 20:40:02 +0200

    cookies

--- toml/__init__.py
+++ toml/__init__.py
@@ -7,6 +7,7 @@ TOML parser to be used on CircuitPython.
 """
 
 __author__ = "elpekenin"
-__version__ = (0, 1, 10)
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/elpekenin/CircuitPython_toml.git"
 
 from ._toml import *
